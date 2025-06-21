246823f189eb9f50a7b5b004b38cb0054a99525c
AymericDfrt <aymericdefoort@outlook.fr>
2025-06-21 12:16:15 +0200

    feat(domain): remove Getter and Setter to Account data

--- bankAccount-domain/src/main/java/fr/kata/bankAccount/domain/data/Account.java
+++ bankAccount-domain/src/main/java/fr/kata/bankAccount/domain/data/Account.java
@@ -1,14 +1,10 @@
 package fr.kata.bankAccount.domain.data;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Data
-@Getter
-@Setter
 public class Account {
 
     private Long accountId;
